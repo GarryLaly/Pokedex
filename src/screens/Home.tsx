ff2930f7cf95fddc72c381477c86d3471460666b
GarryLaly <garrylaly@gmail.com>
2023-08-04 09:15:27 +0700

    feat: setup display svg image url

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, View, StatusBar, Text} from 'react-native';
+import { SvgUri } from 'react-native-svg';
 
 const Home = () => {
   return (
@@ -7,6 +8,11 @@ const Home = () => {
       <StatusBar backgroundColor={'white'} barStyle="dark-content" />
       <View style={styles.container}>
         <Text style={styles.text}>Home</Text>
+        <SvgUri
+          width={200}
+          height={200}
+          uri="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg"
+        />
       </View>
     </SafeAreaView>
   );
